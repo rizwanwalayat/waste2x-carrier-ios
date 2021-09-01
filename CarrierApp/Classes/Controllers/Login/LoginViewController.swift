53d8e4b31e9e5418bebfdd0a8e82ee5f6e6ae29f
Rizwan Walayat <rizwan.wallayat@gmail.com>
2021-09-01 16:53:22 +0500

    Country Picker Implemented

--- CarrierApp/Classes/Controllers/Login/LoginViewController.swift
+++ CarrierApp/Classes/Controllers/Login/LoginViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import ADCountryPicker
 //import LocalAuthentication
 
 class LoginViewController: BaseViewController {
+    
+    
     
     //MARK:- IBOutlets
     @IBOutlet weak var stackView            : UIStackView!
@@ -18,21 +21,25 @@ class LoginViewController: BaseViewController {
     @IBOutlet weak var loginButton          : UIButton!
     @IBOutlet weak var countryIconImgView   : UIImageView!
     @IBOutlet weak var countryDialCodeLbl   : UILabel!
+    @IBOutlet weak var countryCodeView: UIView!
+
     
     
     //MARK:- Variables
     
     var loginViewModel: LoginViewModel?
-    
+    var picker = ADCountryPicker()
+
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationController?.navigationBar.isHidden = true
-        phoneNoTextField.text = "+17734777019"
+        phoneNoTextField.text = "7734777019"
         passwordTextField.text = "123456"
         stackView.spacing = onboardingEstimatedSpcing
-        Utility.selectTextField(countryIconImgView.superview!, isSelected: true)
+        setupCountryPicker()
+        
     }
     
     
@@ -74,10 +81,10 @@ class LoginViewController: BaseViewController {
         self.navigationController?.pushViewController(forgotPasswordVC, animated: true)
     }
     
-    @IBAction func pickupCountryButton(_ sender: Any) {
-        
+    @IBAction func selectCountryBtnPressed(_ sender: Any) {
+      
+        self.present(picker, animated: true, completion: nil)
     }
-    
     
 }
 extension LoginViewController : UITextFieldDelegate {
@@ -85,11 +92,11 @@ extension LoginViewController : UITextFieldDelegate {
    
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
 
-        if textField == phoneNoTextField {
-            if textField.text?.count == 0 && string != "+" {
-                textField.text = "+"
-            }
-        }
+//        if textField == phoneNoTextField {
+//            if textField.text?.count == 0 && string != "+" {
+//                textField.text = "+"
+//            }
+//        }
         
         if phoneNoTextField.text!.count > 0 {
             loginButton.makeEnable(value: true)
@@ -109,5 +116,26 @@ extension LoginViewController : UITextFieldDelegate {
         if textField.isEmpty {
             Utility.selectTextField(textField.superview!, isSelected: false)
         }
+    }
+}
+
+extension LoginViewController: ADCountryPickerDelegate {
+    func countryPicker(_ picker: ADCountryPicker, didSelectCountryWithName name: String, code: String, dialCode: String) {
+        picker.dismiss(animated: true, completion: nil)
+        let flagImage = picker.getFlag(countryCode: code)
+        countryIconImgView.image = flagImage
+        countryDialCodeLbl.text = dialCode
+    }
+    
+    func setupCountryPicker(){
+        
+//        countryCodeView.animateBorderColor(toColor: UIColor.appColor, duration: 0.1)
+        
+        picker.delegate = self
+        picker.showCallingCodes = true
+        picker.defaultCountryCode = "US"
+//        let flagImage = picker.getFlag(countryCode: "US")
+//        countryIconImgView.image = UIImage(named: "US")
+
     }
 }
