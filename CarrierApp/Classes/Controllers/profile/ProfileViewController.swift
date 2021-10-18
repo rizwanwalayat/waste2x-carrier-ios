4a30ee73ffb89d41e494495cddd3329a098fd216
naeemhussain2021 <naeem.hussain@phaedrasolutions.com>
2021-10-18 12:45:21 +0500

    profile UI designed

---
+++ CarrierApp/Classes/Controllers/profile/ProfileViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ProfileViewController.swift
+//  CarrierApp
+//
+//  Created by MAC on 18/10/2021.
+//  Copyright © 2021 codesrbit. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: BaseViewController {
+
+    // MARK: - Outlets
+    
+    
+    
+    // MARK: - Controller's lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+    
+    // MARK: - Actions
+    
+}
