2c2213b6faa263e625073abf62e0795b91e357bf
Rizwan Walayat <rizwan.wallayat@gmail.com>
2021-10-29 17:11:49 +0500

    Added Tests

--- EnMassTransitAPITests/EnMassTransitAPIOnboardTests.swift
+++ EnMassTransitAPITests/EnMassTransitAPIOnboardTests.swift
@@ -48,11 +48,4 @@ class EnMassTransitAPIOnboardTests: XCTestCase {
             }
         }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
